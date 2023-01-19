dcdc1b6fd40be5833fd50dc9683c0f89ba693f07
clo3olb <clo3olb@gmail.com>
2023-01-19 22:33:53 +0800

    extract genHook function

--- schema/hooks.ts
+++ schema/hooks.ts
@@ -4,23 +4,29 @@ import Account from "./Account";
 const ACCOUNT_COLLECTION_NAME = "Accounts";
 
 export async function useAccount(db: Firestore, id: string) {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<Error | null>(null);
-  const [account, setAccount] = useState<Account | null>(null);
-  db.collection(ACCOUNT_COLLECTION_NAME)
-    .doc(id)
-    .onSnapshot(
-      (snapshot) => {
-        const fsAccount = snapshot.data();
-        setAccount(fsAccount ? (fsAccount as Account) : null);
-        setError(null);
-        setLoading(false);
-      },
-      (error) => {
-        setError(error);
-        setAccount(null);
-        setLoading(false);
-      }
-    );
-  return { account, loading, error };
+  return generateHook<Account>()(db, id);
+}
+
+export function generateHook<T>() {
+  return async function (db: Firestore, id: string) {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<Error | null>(null);
+    const [data, setData] = useState<T | null>(null);
+    db.collection(ACCOUNT_COLLECTION_NAME)
+      .doc(id)
+      .onSnapshot(
+        (snapshot) => {
+          const fsDocData = snapshot.data();
+          setData(fsDocData ? (fsDocData as T) : null);
+          setError(null);
+          setLoading(false);
+        },
+        (error) => {
+          setError(error);
+          setData(null);
+          setLoading(false);
+        }
+      );
+    return { data, loading, error };
+  };
 }
