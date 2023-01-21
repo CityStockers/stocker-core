af91d9255730fcb5a3dae1fd68df331520ce3516
clo3olb <clo3olb@gmail.com>
2023-01-22 00:14:38 +0800

    set Type declaration for gen hook

--- schema/genHook.ts
+++ schema/genHook.ts
@@ -1,30 +1,44 @@
-import { Firestore } from "firebase-admin/firestore";
+import {
+  collection,
+  CollectionReference,
+  doc,
+  DocumentData,
+  Firestore,
+  getDoc,
+  onSnapshot,
+} from "firebase/firestore";
 import { useState } from "react";
 
 export const COLLECTION_NAMES = {
   ACCOUNT: "Accounts",
 };
 
-export function genHook<T>() {
+export function genHook<T = DocumentData>() {
   return async function (db: Firestore, id: string) {
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<Error | null>(null);
     const [data, setData] = useState<T | null>(null);
-    db.collection(COLLECTION_NAMES.ACCOUNT)
-      .doc(id)
-      .onSnapshot(
-        (snapshot) => {
-          const fsDocData = snapshot.data();
-          setData(fsDocData ? (fsDocData as T) : null);
-          setError(null);
-          setLoading(false);
-        },
-        (error) => {
-          setError(error);
-          setData(null);
-          setLoading(false);
-        }
-      );
+
+    const collectionRef = collection(
+      db,
+      COLLECTION_NAMES.ACCOUNT
+    ) as CollectionReference<T>;
+    const docRef = doc<T>(collectionRef, id);
+    getDoc(docRef);
+    onSnapshot(
+      docRef,
+      (snapshot) => {
+        const fsDocData = snapshot.data();
+        setData(fsDocData ? (fsDocData as T) : null);
+        setError(null);
+        setLoading(false);
+      },
+      (error) => {
+        setError(error);
+        setData(null);
+        setLoading(false);
+      }
+    );
     return { data, loading, error };
   };
 }
